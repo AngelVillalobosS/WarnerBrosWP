d3ec9434e80707cd23647cbf31807718ffc2448b
AngelVillaloboS <angel.villalobos2269@uppuebla.edu.mx>
2024-11-27 12:41:11 -0600

    Modelos

--- app/Models/detalles_ventas.php
+++ app/Models/detalles_ventas.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class detalles_ventas extends Model
+class Detalles_Ventas extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'detalles_ventas'; // Especifica el nombre de la tabla
+    protected $primaryKey = 'id_venta'; // Clave primaria
+
+    protected $fillable = [
+        'id_venta',
+        'id_producto',
+        'precio_unitario',
+        'cant_devueltas',
+        'cantidad'
+        
+    ];
+
 }
